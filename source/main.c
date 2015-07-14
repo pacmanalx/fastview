8ae765eff51384a7a7a53844603574c6143ade68
Pacman Pereira <pereira@cubicblox.com.br>
2015-07-14 00:34:44 -0300

    Added function to draw a board with borders, clearScreen and centralize board on terminal screen.

--- source/main.c
+++ source/main.c
@@ -3,12 +3,16 @@
 #include <sys/ioctl.h>
 #include <ncurses.h>
 
+#define BOARD_X_SIZE 40
+#define BOARD_Y_SIZE 18
+
 struct winsize terminalSize;
 
 void initScreen(void) {
 	initscr();
 	noecho();
 	nocbreak();
+	start_color();
 }
 
 void closeScreen(void) {
@@ -17,15 +21,56 @@ void closeScreen(void) {
 
 void getScreenSize(void) {
 	if (ioctl(0, TIOCGWINSZ, (char *) &terminalSize) < 0) {
-		printf("The program canno't determine terminal size.\r\n\r\n");
+		printf("The program cannot determine terminal size.\r\n\r\n");
 		exit(1);
 	}
+}
+
+void clearScreen(int backgroundColor) {
+	int x;
+	int y;
+	init_pair(1,7,backgroundColor);
+	attron(COLOR_PAIR(1));
+	for(y=0;y<terminalSize.ws_row;y++) {
+		for(x=0;x<terminalSize.ws_col;x++) {
+			move(y,x);
+			printw(" ");
+		}
+	}
+	attroff(COLOR_PAIR(1));
+}
+
+void drawBoard(void) {
+	int x;
+	int y;
+	int xInitialPos=(terminalSize.ws_col/2)-(BOARD_X_SIZE/2);
+	int yInitialPos=(terminalSize.ws_row/2)-(BOARD_Y_SIZE/2);
+	move(yInitialPos,xInitialPos);
+	addch(ACS_ULCORNER);
+	for(x=1;x<BOARD_X_SIZE;x++) {
+		addch(ACS_HLINE);
+	}
+	addch(ACS_URCORNER);
+	for(y=1;y<BOARD_Y_SIZE;y++) {
+		move(yInitialPos+y,xInitialPos);
+		addch(ACS_VLINE);
+		move(yInitialPos+y,xInitialPos+BOARD_X_SIZE);
+		addch(ACS_VLINE);
+	}
+	move(yInitialPos+BOARD_Y_SIZE,xInitialPos);
+        addch(ACS_LLCORNER);
+        for(x=1;x<BOARD_X_SIZE;x++) {
+                addch(ACS_HLINE);
+        }
+        addch(ACS_LRCORNER);	
 }
 
 int main(void) {
 	initScreen();
 	getScreenSize();
-	move(10,10);
+	clearScreen(3);
+	drawBoard();
+	move(1,1);
 	printw("Terminal size is %dx%d", terminalSize.ws_col, terminalSize.ws_row);
 	getch();
 	closeScreen();
