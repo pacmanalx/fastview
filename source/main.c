bccb5e541c72ce525695738aefacdcfce3ab2541
Pacman Pereira <pereira@cubicblox.com.br>
2015-07-19 23:29:18 -0300

    Added pin description from pin 0 to 13 ...

--- source/main.c
+++ source/main.c
@@ -45,6 +45,8 @@ void drawBoard(void) {
 	int y;
 	int xInitialPos=(terminalSize.ws_col/2)-(BOARD_X_SIZE/2);
 	int yInitialPos=(terminalSize.ws_row/2)-(BOARD_Y_SIZE/2);
+	init_pair(2,7,4);
+	attron(COLOR_PAIR(2));
 	move(yInitialPos,xInitialPos);
 	addch(ACS_ULCORNER);
 	for(x=1;x<BOARD_X_SIZE;x++) {
@@ -52,10 +54,20 @@ void drawBoard(void) {
 	}
 	addch(ACS_URCORNER);
 	for(y=1;y<BOARD_Y_SIZE;y++) {
+		// ======== -- Conectores lado esquerdo
+		move(yInitialPos+y,xInitialPos-2);
+		addch(ACS_HLINE);
+		addch(ACS_HLINE);
 		move(yInitialPos+y,xInitialPos);
 		addch(ACS_VLINE);
+		for(x=1;x<BOARD_X_SIZE;x++) {
+                	addch(' ');
+        	}
 		move(yInitialPos+y,xInitialPos+BOARD_X_SIZE);
 		addch(ACS_VLINE);
+		// ======== -- Conectores do lado direito
+		addch(ACS_HLINE);
+		addch(ACS_HLINE);
 	}
 	move(yInitialPos+BOARD_Y_SIZE,xInitialPos);
         addch(ACS_LLCORNER);
@@ -63,6 +75,24 @@ void drawBoard(void) {
                 addch(ACS_HLINE);
         }
         addch(ACS_LRCORNER);	
+	attroff(COLOR_PAIR(2));
+	
+	attron(COLOR_PAIR(1));
+	move(yInitialPos+ 1, xInitialPos+BOARD_X_SIZE + 4); printw("      13 - ");
+ 	move(yInitialPos+ 2, xInitialPos+BOARD_X_SIZE + 4); printw("      12 - ");
+ 	move(yInitialPos+ 3, xInitialPos+BOARD_X_SIZE + 4); printw("    ~ 11 - ");
+ 	move(yInitialPos+ 4, xInitialPos+BOARD_X_SIZE + 4); printw("    ~ 10 - ");
+ 	move(yInitialPos+ 5, xInitialPos+BOARD_X_SIZE + 4); printw("    ~  9 - ");
+ 	move(yInitialPos+ 6, xInitialPos+BOARD_X_SIZE + 4); printw("       8 - ");
+ 	move(yInitialPos+ 7, xInitialPos+BOARD_X_SIZE + 4); printw("       7 - ");
+ 	move(yInitialPos+ 8, xInitialPos+BOARD_X_SIZE + 4); printw("    ~  6 - ");
+ 	move(yInitialPos+ 9, xInitialPos+BOARD_X_SIZE + 4); printw("    ~  5 - ");
+ 	move(yInitialPos+10, xInitialPos+BOARD_X_SIZE + 4); printw("       4 - ");
+ 	move(yInitialPos+11, xInitialPos+BOARD_X_SIZE + 4); printw("    ~  3 - ");
+ 	move(yInitialPos+12, xInitialPos+BOARD_X_SIZE + 4); printw("       2 - ");
+ 	move(yInitialPos+13, xInitialPos+BOARD_X_SIZE + 4); printw("RX <-  1 - ");
+	move(yInitialPos+14, xInitialPos+BOARD_X_SIZE + 4); printw("RA <-  0 - ");
+	attroff(COLOR_PAIR(1));
 }
 
 int main(void) {
